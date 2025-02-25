d2e95e798a0dc6b8f7a3850905766012682d2a25
Nathanaël Houn <contact@nathanaelhoun.fr>
2025-02-25 16:15:23 +0100

    chore: replace mattermost-server with mattermost/server/public/

--- server/http_dialogs.go
+++ server/http_dialogs.go
@@ -2,36 +2,33 @@ package main
 
 import (
 	"net/http"
-	"strconv"
-
-	"github.com/mattermost/mattermost-server/v5/model"
 )
 
 func (p *Plugin) dialogDeleteLast(w http.ResponseWriter, r *http.Request) {
-	request := model.SubmitDialogRequestFromJson(r.Body)
-	if request == nil {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
+	// request := model.SubmitDialogRequestFromJson(r.Body)
+	// if request == nil {
+	// 	w.WriteHeader(http.StatusBadRequest)
+	// 	return
+	// }
 
-	if request.Cancelled {
-		w.WriteHeader(http.StatusOK)
-		return
-	}
+	// if request.Canceled {
+	// 	w.WriteHeader(http.StatusOK)
+	// 	return
+	// }
 
-	numPostToDelete, err := strconv.Atoi(request.State)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
+	// numPostToDelete, err := strconv.Atoi(request.State)
+	// if err != nil {
+	// 	w.WriteHeader(http.StatusBadRequest)
+	// 	return
+	// }
 
-	w.WriteHeader(http.StatusOK)
+	// w.WriteHeader(http.StatusOK)
 
-	p.deleteLastPostsInChannel(&deletionOptions{
-		channelID:             request.ChannelId,
-		userID:                request.UserId,
-		numPost:               numPostToDelete,
-		optDeletePinnedPosts:  request.Submission["deletePinnedPost"] == "true",
-		permDeleteOthersPosts: canDeleteOthersPosts(p, request.UserId, request.ChannelId),
-	})
+	// p.deleteLastPostsInChannel(&deletionOptions{
+	// 	channelID:             request.ChannelId,
+	// 	userID:                request.UserId,
+	// 	numPost:               numPostToDelete,
+	// 	optDeletePinnedPosts:  request.Submission["deletePinnedPost"] == "true",
+	// 	permDeleteOthersPosts: canDeleteOthersPosts(p, request.UserId, request.ChannelId),
+	// })
 }
